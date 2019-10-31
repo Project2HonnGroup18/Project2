8a3cad41eb898a3eb5663280931ebfc70d66c57f
ValgardurRI <valgardur16@ru.is>
2019-10-31 21:06:28 +0000

    Added several integration tests

--- AcademicReferenceManager.Tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ AcademicReferenceManager.Tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -13,18 +13,14 @@ using AcademicReferenceManager.Repositories.Implementations;
 using AcademicReferenceManager.Repositories.Seeding;
 using System.Linq;
 
-public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+public class CustomWebApplicationFactory<TStartup> :  WebApplicationFactory<TStartup> where TStartup: class
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
-            // Create a new service provider.
-            var serviceProvider = new ServiceCollection()
-                .AddEntityFrameworkInMemoryDatabase()
-                .BuildServiceProvider();
-
-            // Remove the app's ApplicationDbContext registration.
+            var serviceProvider = new ServiceCollection().AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+            // Remove the app's DbContext registration.
             var descriptor = services.SingleOrDefault(
                 d => d.ServiceType == 
                     typeof(DbContextOptions<ArmDbContext>));
@@ -33,6 +29,7 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Start
             {
                 services.Remove(descriptor);
             }
+            services.AddEntityFrameworkInMemoryDatabase();
             
             // Add ApplicationDbContext using an in-memory database for testing.
             services.AddDbContext<ArmDbContext>((options) => 
@@ -40,32 +37,38 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Start
                 options.UseInMemoryDatabase("InMemoryDbForTesting");
                 options.UseInternalServiceProvider(serviceProvider);
             });
-
-
+            
+            
             // Build the service provider.
             var sp = services.BuildServiceProvider();
 
             using (var scope = sp.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<ArmDbContext>();
+                var logger = scopedServices
+                    .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+                
+                // Wait for database to get created
+                db.Database.EnsureCreated();
+
+                // Ensure the database is created.
+                try
                 {
-                    var scopedServices = scope.ServiceProvider;
-                    var db = scopedServices.GetRequiredService<ArmDbContext>();
-                    var logger = scopedServices
-                        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
-
-                    // Ensure the database is created.
-                    db.Database.EnsureCreated();
-
-                    try
-                    {
-                        // Seed the database with test data.
-                        DatabaseSeeder seeder = new DatabaseSeeder();
-                        seeder.seedDatabase(db);
-                    }
-                    catch (Exception ex)
-                    {
-                        logger.LogError(ex, "An error occurred seeding the database. Error: {Message}", ex.Message);
-                    }
+                    DatabaseSeeder seeder = new DatabaseSeeder();
+                    seeder.seedDatabase(db);
+                    Console.WriteLine(db.Friends.FirstOrDefault().FirstName);
                 }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred seeding the " +
+                        "database with test messages. Error: {Message}", ex.Message);
+                }
+            }
         });
+    }
+    public ArmDbContext GetContext()
+    {
+        return Server.Host.Services.CreateScope().ServiceProvider.GetService<ArmDbContext>();
     }
 }
